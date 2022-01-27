db18dc836b5de600161d09dbbf802700e58854fc
Hailey Stahl <hailey.stahl@gmail.com>
2022-01-27 17:40:49 -0600

    1/27/2022 6:00pm

--- SCRIPT.R
+++ SCRIPT.R
@@ -1,2 +1,29 @@
+
+#Other libraries
+
+library(ggfortify)
 library(readxl)
+library(dplyr)     
+#viz libraries
+library(ggplot2)
+library(corrplot)
 data <- read_excel("C:/Users/haile/OneDrive - Texas Lutheran University/Desktop/GitProjectsTLU/Project1/CLEANEDDATASET.xlsx")
+View(data)
+# Median and Mean together, meaning no outliers
+# Spread across
+# y outcome trying to measure
+summary(data$ftretention_rate)
+
+#Vizulize y outcome
+histogram= ggplot(data, aes(x=ftretention_rate)) +    
+  
+geom_histogram(binwidth=.05,color="white",fill ="#DFA1A1")+
+  labs( x = "Retention Rate",y = "Count",size = 30)
+
+print(histogram)
+
+
+
+
+
+cor(data$tuition_reliance_a1,data$tuition_reliance_a2)
---
+++ ScrapCode.R
@@ -0,0 +1,13 @@
+
+
+#PCA : Data set has a lot of variables
+#Use PCA on only numeric columns in data set
+#data_num <- select_if(data, is.numeric) 
+#data_num<-subset(data_num)
+#Running PCA
+#
+
+#pcanum<- prcomp(na.omit(data_num))
+#summary(pcanum)
+
+#pcanum$rot[,1:50]
