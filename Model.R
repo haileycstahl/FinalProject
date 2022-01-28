5783d2c7465fcca8a82794af4b8f854f17033d53
Hailey Stahl <hailey.stahl@gmail.com>
2022-01-28 15:09:57 -0600

    Renaming ID col

--- Model.R
+++ Model.R
@@ -1,10 +1,10 @@
 library(tidyverse)
-
+datam <- read_csv("finaldataclean.csv",  col_types = cols(X1 = col_number()))
 #getting rid of variables not wanting to use for model
 datam<-subset(data,select=-c(academicyear,instname))
 
 #Renaming for an ID column 
 #
-
 View(datam)
+
 
